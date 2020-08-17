5f4179ca4d79b5394f2b3e0106f12521066e5729
e <1735919138@qq.com>
2020-08-17 23:21:22 +0800

    前台项目框架

---
+++ src/main/java/com/aaa/controller/Test.java
@@ -0,0 +1,12 @@
+package com.aaa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Test {
+    @RequestMapping("main")
+    public String showMain(){
+        return "main";
+    }
+}
